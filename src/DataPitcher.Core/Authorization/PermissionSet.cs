16435914fedd62ab5d5d229c0c0003a17e3bbb6e
emptycoder <yaroslav200124@gmail.com>
2026-09-02 09:37:56 +0200

    feat: add authorization permission bundles

---
+++ src/DataPitcher.Core/Authorization/PermissionSet.cs
@@ -0,0 +1,30 @@
+namespace DataPitcher.Core.Authorization;
+
+public sealed class PermissionSet : IEquatable<PermissionSet>
+{
+    private readonly HashSet<Permission> permissions;
+
+    public PermissionSet(IEnumerable<Permission> permissions)
+    {
+        this.permissions = [.. permissions];
+        Permissions = Array.AsReadOnly(this.permissions.OrderBy(permission => permission.Value, StringComparer.Ordinal).ToArray());
+    }
+
+    public static PermissionSet Empty { get; } = new([]);
+
+    public IReadOnlyCollection<Permission> Permissions { get; }
+
+    public bool Contains(Permission permission) => permissions.Contains(permission);
+
+    public PermissionSet Union(PermissionSet other) => new(permissions.Concat(other.permissions));
+
+    public PermissionSet Without(Permission permission) => new(permissions.Where(candidate => candidate != permission));
+
+    public bool IsSubsetOf(PermissionSet other) => permissions.IsSubsetOf(other.permissions);
+
+    public bool Equals(PermissionSet? other) => other is not null && permissions.SetEquals(other.permissions);
+
+    public override bool Equals(object? obj) => obj is PermissionSet other && Equals(other);
+
+    public override int GetHashCode() => permissions.Aggregate(0, (hash, permission) => hash ^ permission.GetHashCode());
+}
